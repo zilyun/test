b08f9fc829ad51400371fcc072c2af306f84ad38
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-02-29 11:38:58 +0900

    first commit

---
+++ src/test/Solution.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int[][] land = { { 1, 2, 9, 8 }, { 5, 6, 9, 7 }, { 4, 3, 3, 1 } };
+		solution(land);
+
+	}
+
+	static int solution(int[][] land) {
+		int answer = 0;
+
+		for (int i = 1; i < land.length; i++) {
+			land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+			land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+			land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+			land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+
+			System.out
+					.println(land[i - 1][0] + " : " + land[i - 1][1] + " : " + land[i - 1][2] + " : " + land[i - 1][3]);
+			System.out.println(land[i][0] + " : " + land[i][1] + " : " + land[i][2] + " : " + land[i][3]);
+		}
+
+		for (int i : land[land.length - 1]) {
+			answer = Math.max(answer, i);
+		}
+
+		return answer;
+	}
+}
